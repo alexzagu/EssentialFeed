fec435becc7299d7d915b0ddee81a670a67746bc
Alejandro Zamudio Guajardo <>
2020-06-10 23:27:28 +0200

    Created FeedItem model.

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,18 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Alejandro Zamudio Guajardo on 10/06/2020.
+//  Copyright © 2020 MWare. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+
+}
