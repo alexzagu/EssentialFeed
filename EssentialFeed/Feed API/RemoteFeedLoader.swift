d0c4d6df9632f74dfe87b330efd899b5a7e6dd19
Alejandro Zamudio Guajardo <>
2020-06-11 20:42:17 +0200

    Delivers invalidData error on non 200 HTTP responses.

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public protocol HTTPClient {
 
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 
 }
 
@@ -21,6 +21,7 @@ public final class RemoteFeedLoader {
 
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
 
     public init(url: URL, client: HTTPClient) {
@@ -29,8 +30,12 @@ public final class RemoteFeedLoader {
     }
 
     public func load(completion: @escaping (Error) -> Void) {
-        self.client.get(from: self.url) { error in
-            completion(.connectivity)
+        self.client.get(from: self.url) { error, response in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
         }
     }
 
