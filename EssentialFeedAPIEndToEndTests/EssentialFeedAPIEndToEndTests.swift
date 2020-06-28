c896fff16d282c7ae25ffe40947911bf09dc5a2d
Alejandro Zamudio Guajardo <>
2020-06-28 22:50:36 +0200

    Extract test setup to a helper method.

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -12,21 +12,7 @@ import XCTest
 class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
-
-        let expectation = self.expectation(description: "Wait for load completion.")
-
-        var receivedResult: RemoteFeedLoader.Result?
-        loader.load { result in
-            receivedResult = result
-            expectation.fulfill()
-        }
-
-        self.wait(for: [expectation], timeout: 5.0)
-
-        switch receivedResult {
+        switch self.getFeedResult() {
         case let .success(items):
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed.")
             XCTAssertEqual(items[0], self.expectedItem(at: 0))
@@ -47,6 +33,24 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
 
     // MARK: - Helpers
+
+    private func getFeedResult() -> RemoteFeedLoader.Result? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+
+        let expectation = self.expectation(description: "Wait for load completion.")
+
+        var receivedResult: RemoteFeedLoader.Result?
+        loader.load { result in
+            receivedResult = result
+            expectation.fulfill()
+        }
+
+        self.wait(for: [expectation], timeout: 5.0)
+
+        return receivedResult
+    }
 
     private func expectedItem(at index: Int) -> FeedItem {
         FeedItem(id: self.id(at: index), description: self.description(at: index), location: self.location(at: index), imageURL: self.imageURL(at: index))
